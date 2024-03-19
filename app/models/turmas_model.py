e54746e164a92809465b866ad47814e61ddeb696
Valtair Junior <valtair.developer@gmail.com>
2024-03-19 15:35:11 -0400

    adiciona dinamica de horarios em turma e remome turno e ano

--- app/models/turmas_model.py
+++ app/models/turmas_model.py
@@ -8,8 +8,8 @@ class Turmas(Base):
     disciplina = db.Column(db.String)
     sala = db.Column(db.UUID(as_uuid=True), db.ForeignKey("salas.uuid"))
     nome_professor = db.Column(db.String)
-    turno = db.Column(db.String)
-    horario = db.Column(db.Time)
-    ano = db.Column(db.Date)
+    horario = db.Column(db.Text)
     sala_turma = db.relationship("Salas", back_populates="turmas")
     aluno_tumas = db.relationship("AlunoTurmas", back_populates="turmas")
+
+    
